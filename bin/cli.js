ec01a109614abb9448593ed65e5bad0185cc69c6
Andrew Todd <emailandytodd@gmail.com>
2024-11-09 15:01:00 +0000

    Slight refactor + dependabot

--- bin/cli.js
+++ bin/cli.js
@@ -1,33 +1,47 @@
 #!/usr/bin/env node
 
-import { handleInit } from "../lib/init.js";
+import {  handleAuthorise } from "../lib/authorise.js";
 import { handleTransactions } from "../lib/transactions.js";
 import { program } from "commander";
 import Configstore from "configstore";
+import chalk from 'chalk';
 
 const config = new Configstore("ebay-cli");
+
+const checkAuth = (cmd) => {
+  if (!config.get("auth")) {
+    console.error(chalk.red('Authentication required!'));
+    console.error(chalk.yellow('Please run:'));
+    console.error(chalk.cyan('ebay authorise <clientId> <certificateId> <redirectUrlName>'));
+    process.exit(1);
+  }
+};
+
 const app = program.version("0.1.0", "-v, --version");
 
+// Commands that don't need auth
 app
   .command("config")
   .alias("c")
   .action(() => console.log(`Config file located at: ${config.path}`));
 
 app
-  .command("init")
-  .alias("i")
+  .command("authorise")
+  .alias("auth")
   .description("Get auth token to access eBay API")
   .argument("<clientId>", "eBay developer account client-id")
   .argument("<certificateId>", "eBay developer account certificate-id")
   .argument("<redirectUrlName>", "eBay developer account ruName")
   .action((clientId, certificateId, redirectUrlName) =>
-    handleInit({ clientId, certificateId, redirectUrlName, config })
+    handleAuthorise({ clientId, certificateId, redirectUrlName, config })
   );
 
+// Commands that need auth
 app
   .command("transactions")
   .alias("tx")
   .description("Retrieve transaction data")
+  .hook('preAction', checkAuth)
   .option("-f --date-from <string>", "Transactions from this date (ISO)")
   .option("-t --date-to <string>", "Transactions up until this date (ISO)")
   .option("-y --type <string>", "Transactions type")
